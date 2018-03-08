2d1096273c7d641d49a3f9e5c5cd98b1a6a07b76
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 17:24:19 -0500

    Shell Command - Modified commented out shell command

--- scripts/auth/sign-in.sh
+++ scripts/auth/sign-in.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# EMAIL="" PASSWORD="" PASSWORD_CONFIRMATION="" sh scripts/auth/sign-in.sh
+# EMAIL="" PASSWORD="" sh scripts/auth/sign-in.sh
 
 curl "https://rank-it-up.herokuapp.com/sign-in" \
 --include \
