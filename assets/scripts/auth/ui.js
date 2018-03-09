1deed834aacb5d39e222b128f3668dccf9948e70
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 20:59:14 -0500

    UI: Functions - Added empty functions for requests

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,11 +5,35 @@ const store = require('../store')
 const signUpSuccess = function (data) {
 }
 
+const signUpFailure = function (error) {
+}
+
 const signInSuccess = function (data) {
   store.user = data.user
 }
 
+const signInFailure = function (error) {
+}
+
+const changePasswordSuccess = function (data) {
+}
+
+const changePasswordFailure = function (error) {
+}
+
+const signOutSuccess = function (data) {
+}
+
+const signOutFailure = function (error) {
+}
+
 module.exports = {
   signUpSuccess,
-  signInSuccess
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
 }
