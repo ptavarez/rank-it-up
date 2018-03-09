52b4f85bc74a97b5620e0e882faa40b7ce0c2cb0
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 20:02:04 -0500

    Events: Sign In - Successfully create an events function to sign in

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -13,8 +13,17 @@ const onSignUp = function (event) {
     .catch(ui.signUpFailure)
 }
 
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
 const addHandlers = () => {
   $('.sign-up-form').on('submit', onSignUp)
+  $('.sign-in-form').on('submit', onSignIn)
 }
 
 module.exports = {
