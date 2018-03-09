4781cc7d4eb46b5b7f1f70be78cca82c2dd4e536
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 19:06:28 -0500

    Client Auth - Created events.js file - Created sign-up events function

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const api = require('./api')
+const getFormFields = require('../../../lib/get-form-fields')
+const ui = require('./ui')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+
+  const data = getFormFields(this)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const addHandlers = () => {
+  $('.sign-up-form').on('submit', onSignUp)
+}
+
+module.exports = {
+  addHandlers
+}
