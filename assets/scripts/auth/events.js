b65a10f6ff0d50465655ac592d2a962db5babb45
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 20:58:06 -0500

    Auth Events: Sign out - Created function onSignOut to sign out of application

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -29,10 +29,18 @@ const onChangePassword = function (event) {
     .catch(ui.changePasswordFailure)
 }
 
+const onSignOut = function (event) {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
 const addHandlers = () => {
   $('.sign-up-form').on('submit', onSignUp)
   $('.sign-in-form').on('submit', onSignIn)
   $('.change-password-form').on('submit', onChangePassword)
+  $('#sign-out').on('submit', onSignOut)
 }
 
 module.exports = {
