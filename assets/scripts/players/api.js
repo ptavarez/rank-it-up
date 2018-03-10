05e124b80d8fcf9545e6508cbd89588182238a6c
ptavarez16 <ptavarez16@yahoo.com>
2018-03-10 13:43:42 -0500

    API: Create Player - createPlayer function to send an api request to post a new player

---
+++ assets/scripts/players/api.js
@@ -0,0 +1,20 @@
+const config = require('../config')
+const store = require('../store')
+
+
+const createPlayer = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/players/',
+    method: 'POST',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+      player: {
+        wins: 0,
+        losses: 0
+      }
+    }
+  })
+}
