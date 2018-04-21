36964b66716e84be68cab6af53468366abbdb51b
ptavarez16 <ptavarez16@yahoo.com>
2018-04-21 19:03:33 -0400

    New create match script
    - New script folder: matches
    - Add create match script

---
+++ scripts/matches/create-match.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# TOKEN= DESCRIPTION="Test Duel" CHALLENGER=Dro OPPONENT=Gabe WINNER=Gabe LOSER=Dro sh  scripts/matches/create-match.sh
+
+curl "http://localhost:4741/matches" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "match": {
+      "description": "'"${DESCRIPTION}"'",
+      "challenger": "'"${CHALLENGER}"'",
+      "opponent": "'"${OPPONENT}"'",
+      "winner": "'"${WINNER}"'",
+      "loser": "'"${LOSER}"'"
+    }
+  }'
+
+  echo
