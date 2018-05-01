85ecd059159d1bcf3eaa5689b264b578a5d2a275
ptavarez16 <ptavarez16@yahoo.com>
2018-05-01 18:50:30 -0400

    Remove all unnecessary code

--- assets/scripts/matches/api.js
+++ assets/scripts/matches/api.js
@@ -15,51 +15,6 @@ const createMatch = function (data) {
   })
 }
 
-const getMatches = function () {
-  return $.ajax({
-    url: config.apiOrigin + '/matches/',
-    method: 'GET',
-    headers: {
-      contentType: 'application/json',
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
-}
-
-const getMatch = function (id) {
-  return $.ajax({
-    url: config.apiOrigin + '/matches/' + id,
-    method: 'GET',
-    headers: {
-      contentType: 'application/json',
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
-}
-
-const updateMatch = function (data) {
-  return $.ajax({
-    url: config.apiOrigin + '/matches/' + data.player.id,
-    method: 'PATCH',
-    headers: {
-      contentType: 'application/json',
-      Authorization: 'Token token=' + store.user.token
-    },
-    data
-  })
-}
-
-const deleteMatch = function (id) {
-  return $.ajax({
-    url: config.apiOrigin + '/matches/' + id,
-    method: 'DELETE',
-    headers: {
-      contentType: 'application/json',
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
-}
-
 const listPlayers = function () {
   return $.ajax({
     url: config.apiOrigin + '/players/',
@@ -73,9 +28,5 @@ const listPlayers = function () {
 
 module.exports = {
   createMatch,
-  getMatches,
-  getMatch,
-  updateMatch,
-  deleteMatch,
   listPlayers
 }
